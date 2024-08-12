1bf3d205155e1157da54bd80c905415ae778a944
farfun <1007530194@qq.com>
2024-08-12 10:14:59 +0800

    add

--- src/fundrive/drives/webdav/drive.py
+++ src/fundrive/drives/webdav/drive.py
@@ -3,16 +3,22 @@ import subprocess
 from typing import Any, List, Dict
 
 from fundrive.core import DriveSystem
+from funsecret import read_secret
 
 
 class WebDavDrive(DriveSystem):
     def __init__(self, *args, **kwargs):
         super(WebDavDrive, self).__init__(*args, **kwargs)
-        from webdav4.client import Client
-
-        self.drive = None  # Client("")
+        self.drive = None
 
     def login(self, server_url, username, password, *args, **kwargs) -> bool:
+        server_url = server_url or read_secret(
+            "fundrive", "webdav", "alipan", "server_url"
+        )
+        username = username or read_secret("fundrive", "webdav", "alipan", "username")
+        password = password or read_secret("fundrive", "webdav", "alipan", "password")
+        if not server_url or not username or not password:
+            raise Exception("server_url, username, password is None")
         try:
             from webdav4.client import Client
         except Exception as e:
@@ -30,10 +36,12 @@ class WebDavDrive(DriveSystem):
         return True
 
     def get_file_list(self, path, *args, **kwargs) -> List[Dict[str, Any]]:
-        return self.drive.ls(path=path)
+        return [file for file in self.drive.ls(path=path) if file["type"] == "file"]
 
     def get_dir_list(self, path, *args, **kwargs) -> List[Dict[str, Any]]:
-        return self.drive.ls(path)
+        return [
+            file for file in self.drive.ls(path=path) if file["type"] == "directory"
+        ]
 
     def get_file_info(self, path, *args, **kwargs) -> Dict[str, Any]:
         return self.drive.info(path)
@@ -44,6 +52,7 @@ class WebDavDrive(DriveSystem):
     def download_file(
         self, dir_path="./cache", path=None, overwrite=False, *args, **kwargs
     ) -> bool:
+        os.makedirs(dir_path, exist_ok=True)
         self.drive.download_file(
             from_path=path, to_path=f"{dir_path}/{os.path.basename(path)}"
         )
@@ -52,6 +61,8 @@ class WebDavDrive(DriveSystem):
     def download_dir(
         self, dir_path="./cache", path=None, overwrite=False, *args, **kwargs
     ) -> bool:
+        for file in self.get_file_list(path):
+            self.download_file(dir_path=f"{dir_path}/{file['name']}", path=file["name"])
         return True
 
     def upload_file(
@@ -66,10 +77,3 @@ class WebDavDrive(DriveSystem):
         self, file_path="./cache", path=None, overwrite=False, *args, **kwargs
     ) -> bool:
         return True
-
-
-drive = WebDavDrive()
-drive.login(
-    server_url="https://openapi.alipan.com", username="fwWsC4", password="jfm6A3"
-)
-print(drive.get_file_list(""))
