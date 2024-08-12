7fcccecc74dd706bf7a7b9308a65a43bca8132d1
farfun <1007530194@qq.com>
2024-08-12 14:30:25 +0800

    add

---
+++ src/fundrive/drives/os.py
@@ -0,0 +1,47 @@
+import os
+import shutil
+from typing import Any, List, Dict
+
+from fundrive.core import BaseDrive
+
+
+class OSDrive(BaseDrive):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def exist(self, path, *args, **kwargs) -> bool:
+        return os.path.exists(path)
+
+    def mkdir(self, path, exist_ok=True, *args, **kwargs) -> bool:
+        os.makedirs(path, exist_ok=exist_ok)
+        return True
+
+    def upload_file(
+        self, local_path, drive_path, recursion=True, overwrite=False, *args, **kwargs
+    ) -> bool:
+        print(local_path, drive_path)
+        shutil.copyfile(local_path, drive_path)
+        return True
+
+    def download_file(
+        self, local_path, drive_path, overwrite=False, *args, **kwargs
+    ) -> bool:
+        print(local_path, drive_path)
+        shutil.copyfile(drive_path, local_path)
+        return True
+
+    def get_file_list(self, path, *args, **kwargs) -> List[Dict[str, Any]]:
+        result = []
+        for file in os.listdir(path):
+            file_path = os.path.join(path, file)
+            if os.path.isfile(file_path):
+                result.append({"path": file_path})
+        return result
+
+    def get_dir_list(self, path, *args, **kwargs) -> List[Dict[str, Any]]:
+        result = []
+        for file in os.listdir(path):
+            file_path = os.path.join(path, file)
+            if os.path.isdir(file_path):
+                result.append({"path": file_path})
+        return result
