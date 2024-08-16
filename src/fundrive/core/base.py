6eb6d67ab0c5580fce148e6adc96eec88ec1015d
farfun <1007530194@qq.com>
2024-08-16 16:33:02 +0800

    add

--- src/fundrive/core/base.py
+++ src/fundrive/core/base.py
@@ -1,5 +1,41 @@
 import os
-from typing import Any, List, Dict
+from typing import List
+
+
+class DriveFile(dict):
+    def __init__(self, fid, name, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fid = fid
+        self.name = name
+        self.data = {"fid": fid, "name": name}
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
+
+    def __delitem__(self, key):
+        del self.data[key]
+
+    def __contains__(self, key):
+        return key in self.data
+
+    def keys(self):
+        return list(self.data.keys())
+
+    def values(self):
+        return list(self.data.values())
+
+    def items(self):
+        return list(self.data.items())
+
+    def clear(self):
+        self.data.clear()
+
+    @property
+    def size(self):
+        return len(self.data)
 
 
 class BaseDrive:
@@ -15,77 +51,76 @@ class BaseDrive:
         """
         raise NotImplementedError()
 
-    def mkdir(self, path, exist_ok=True, *args, **kwargs) -> bool:
+    def mkdir(self, fid, name, return_if_exist=True, *args, **kwargs) -> str:
         """
-        创建目录
-        :param exist_ok:
-        :param path:
+        创建目录，如果目录存在则返回目录id
+        :param return_if_exist:
+        :param fid:
+        :param name:
         :param args:
         :param kwargs:
         :return:
         """
         raise NotImplementedError()
 
-    def exist(self, path, *args, **kwargs) -> bool:
+    def exist(self, fid, *args, **kwargs) -> bool:
         raise NotImplementedError()
 
-    def delete(self, path, *args, **kwargs) -> bool:
+    def delete(self, fid, *args, **kwargs) -> bool:
         """
         删除
-        :param path:
+        :param fid:
         :param args:
         :param kwargs:
         :return:
         """
         raise NotImplementedError()
 
-    def get_file_list(self, path, *args, **kwargs) -> List[Dict[str, Any]]:
+    def get_file_list(self, fid, *args, **kwargs) -> List[DriveFile]:
         """
         获取文件列表
-        :param path:
+        :param fid:
         :param args:
         :param kwargs:
         :return:
         """
         raise NotImplementedError()
 
-    def get_dir_list(self, path, *args, **kwargs) -> List[Dict[str, Any]]:
+    def get_dir_list(self, fid, *args, **kwargs) -> List[DriveFile]:
         """
         获取目录列表
-        :param path:
+        :param fid:
         :param args:
         :param kwargs:
         :return:
         """
         raise NotImplementedError()
 
-    def get_file_info(self, path, *args, **kwargs) -> Dict[str, Any]:
+    def get_file_info(self, fid, *args, **kwargs) -> DriveFile:
         """
         获取文件信息
-        :param path:
+        :param fid:
         :param args:
         :param kwargs:
         :return:
         """
         raise NotImplementedError()
 
-    def get_dir_info(self, path, *args, **kwargs) -> Dict[str, Any]:
+    def get_dir_info(self, fid, *args, **kwargs) -> DriveFile:
         """
         获取目录信息
-        :param path:
+        :param fid:
         :param args:
         :param kwargs:
         :return:
         """
         raise NotImplementedError()
 
-    def download_file(
-        self, local_path, drive_path, overwrite=False, *args, **kwargs
-    ) -> bool:
+    def download_file(self, fid, local_dir, overwrite=False, *args, **kwargs) -> bool:
         """
         下载文件
-        :param local_path:
-        :param drive_path:
+        :param fid:
+        :param local_dir:
         :param overwrite:
         :param args:
         :param kwargs:
@@ -94,41 +129,34 @@ class BaseDrive:
         raise NotImplementedError()
 
     def download_dir(
-        self, local_path, drive_path, recursion=True, overwrite=False, *args, **kwargs
+        self, fid, local_dir, recursion=True, overwrite=False, *args, **kwargs
     ) -> bool:
         """
         下载目录
-        :param local_path:
-        :param drive_path:
+        :param fid:
+        :param local_dir:
          :param recursion:
         :param overwrite:
         :param args:
         :param kwargs:
         :return:
         """
-        if not self.exist(drive_path):
+        if not self.exist(fid):
             return False
-        if not os.path.exists(local_path):
-            os.makedirs(local_path, exist_ok=True)
-        for file in self.get_file_list(drive_path):
-            _drive_path = file["path"]
-            _local_path = os.path.join(local_path, os.path.basename(_drive_path))
+        if not os.path.exists(local_dir):
+            os.makedirs(local_dir, exist_ok=True)
+        for file in self.get_file_list(fid):
+            _drive_path = file["fid"]
             self.download_file(
-                local_path=_local_path,
-                drive_path=_drive_path,
-                overwrite=overwrite,
-                *args,
-                **kwargs
+                fid=file.fid, local_dir=local_dir, overwrite=overwrite, *args, **kwargs
             )
         if not recursion:
             return True
 
-        for file in self.get_dir_list(drive_path):
-            _drive_path = file["path"]
-            _local_path = os.path.join(local_path, os.path.basename(_drive_path))
+        for file in self.get_dir_list(fid):
             self.download_dir(
-                local_path=_local_path,
-                drive_path=_drive_path,
+                fid=file.fid,
+                local_dir=os.path.join(local_dir, os.path.basename(fid.name)),
                 overwrite=overwrite,
                 recursion=recursion,
                 *args,
@@ -136,14 +164,14 @@ class BaseDrive:
             )
 
     def upload_file(
-        self, local_path, drive_path, recursion=True, overwrite=False, *args, **kwargs
+        self, local_path, fid, recursion=True, overwrite=False, *args, **kwargs
     ) -> bool:
         """
         上传文件
         :param local_path:
-        :param drive_path:
+        :param fid:
         :param recursion:
-        :param overwrite:
+            :param overwrite:
         :param args:
         :param kwargs:
         :return:
@@ -151,39 +179,16 @@ class BaseDrive:
         raise NotImplementedError()
 
     def upload_dir(
-        self, local_path, drive_path, recursion=True, overwrite=False, *args, **kwargs
+        self, local_path, fid, recursion=True, overwrite=False, *args, **kwargs
     ) -> bool:
         """
         上传目录
         :param local_path:
-        :param drive_path:
+        :param fid:
          :param recursion:
         :param overwrite:
         :param args:
         :param kwargs:
         :return:
         """
-        if not os.path.exists(local_path):
-            return False
-        for file in os.listdir(local_path):
-            _local_path = os.path.join(local_path, file)
-            _drive_path = os.path.join(drive_path, file)
-            if not self.exist(_drive_path):
-                self.mkdir(drive_path)
-            if os.path.isfile(_local_path):
-                self.upload_file(
-                    local_path=_local_path,
-                    drive_path=_drive_path,
-                    overwrite=overwrite,
-                    *args,
-                    **kwargs
-                )
-            elif recursion:
-                self.upload_dir(
-                    local_path=_local_path,
-                    drive_path=_drive_path,
-                    recursion=recursion,
-                    overwrite=overwrite,
-                    *args,
-                    **kwargs
-                )
+        raise NotImplementedError()
