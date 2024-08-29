4eb6f89356d67611036a52ca75c5ea61e0bfa9fe
farfun <1007530194@qq.com>
2024-08-29 19:33:26 +0800

    add

--- src/fundrive/core/table.py
+++ src/fundrive/core/table.py
@@ -2,24 +2,34 @@ import json
 import logging
 import os.path
 
-from fundrive import LanZouDrive
 from fundrive.core import BaseDrive
+
+logger = logging.getLogger("fundrive")
 
 
 class DriveTable:
-    def __init__(self, table_fid, drive: BaseDrive, drive_root=""):
+    def __init__(self, table_fid, drive: BaseDrive):
         self.table_fid = table_fid
-        self.drive_root = drive_root
         self.drive = drive
+
         self._fid_par_dict = {}
         self._fid_meta = None
-        self._fid_meta_partition_json = None
+        self._fid_meta_fid = None
 
     @property
-    def __meta_partition_json(self):
-        return "./partition.tar"
+    def __local_meta_path(self):
+        cache_dir = f"{os.environ['HOME']}/.cache/fundrive/table"
+        os.makedirs(cache_dir, exist_ok=True)
+        return f"{cache_dir}/{self.table_fid}/partition.tar"
 
-    def cache_partition(self):
+    @property
+    def meta_path(self):
+        if len(self._fid_par_dict) == 0:
+            self.update_partition_dict()
+
+        return self._fid_meta
+
+    def update_partition_dict(self):
         for file in self.drive.get_dir_list(self.table_fid):
             self._fid_par_dict[file["name"]] = file["fid"]
 
@@ -29,17 +39,10 @@ class DriveTable:
             self._fid_meta = self.drive.mkdir(fid=self.table_fid, name="_meta")
 
         for file in self.drive.get_file_list(self._fid_meta):
-            if file["name"] == os.path.basename(self.__meta_partition_json):
-                self._fid_meta_partition_json = file["fid"]
-        print(f"partition_size={len(self._fid_par_dict)}")
-        print(f" _fid_meta_partition_json={self._fid_meta_partition_json}")
-
-    @property
-    def meta_path(self):
-        if len(self._fid_par_dict) == 0:
-            self.cache_partition()
-
-        return self._fid_meta
+            if file["name"] == os.path.basename(self.__local_meta_path):
+                self._fid_meta_fid = file["fid"]
+        logger.info(f"partition_size={len(self._fid_par_dict)}")
+        logger.info(f"fid_meta_fid={self._fid_meta_fid}")
 
     def upload(self, file, partition):
         if partition in self._fid_par_dict.keys():
@@ -47,40 +50,45 @@ class DriveTable:
         else:
             logging.info(f" partition={partition} not exists,create it.")
             fid = self.drive.mkdir(fid=self.table_fid, name=partition)
-            self.cache_partition()
+            self.update_partition_dict()
         self.drive.upload_file(fid=fid, local_path=file)
 
-    def update_partition_meta(self, refresh=False, *args, **kwargs):
-        tmp = self.__meta_partition_json
-        print(f"download {self.__meta_partition_json}")
-        if self._fid_meta_partition_json is not None:
-            self.drive.download_file(fid=self._fid_meta_partition_json, local_dir="./")
-        partition_meta = []
-        if os.path.exists(tmp):
-            with open(tmp, "r") as f:
-                partition_meta = json.load(f)
+    def update_partition_meta(self, *args, **kwargs):
+        local_meta_path = self.__local_meta_path
+
+        logger.info(f"download meta from drive to {local_meta_path}.")
+        if self._fid_meta_fid is not None:
+            self.drive.download_file(fid=self._fid_meta_fid, local_dir="./")
+
+        partition_meta = self.partition_meta(refresh=False)
+        logger.info(f"exists meta size={len(partition_meta)}")
 
         partition_meta = dict([file["name"], file] for file in partition_meta)
         for partition_name, partition_fid in self._fid_par_dict.items():
             if partition_name.startswith("_"):
                 continue
 
-            for file in self.drive.get_file_list(partition_fid):
-                partition_meta[file["name"]] = file.fid
-        print(f"file size={len(partition_meta)}")
-        with open(tmp, "w") as f:
-            json.dump(list(partition_meta.values()), f)
-        self.drive.upload_file(local_path=tmp, fid=self._fid_meta_partition_json)
-        os.remove(tmp)
+            for file in self.drive.get_file_list(fid=partition_fid):
+                partition_meta[file["name"]] = file
 
-    def partition_meta(self):
-        tmp = self.__meta_partition_json
+        logger.info(f"update meta size={len(partition_meta)}")
+        logger.info(f"upload meta file")
+        json.dump(list(partition_meta.values()), open(local_meta_path, "w"))
+        self.drive.upload_file(local_path=local_meta_path, fid=self._fid_meta_fid)
+
+        logger.info(f"remove meta file")
+        os.remove(local_meta_path)
+
+    def partition_meta(self, refresh=False):
+        tmp = self.__local_meta_path
+
+        if not os.path.exists(tmp) or refresh:
+            if os.path.exists(tmp):
+                os.remove(tmp)
+
+            if self._fid_meta_fid is not None:
+                self.drive.download_file(local_dir="./", fid=self._fid_meta_fid)
         if os.path.exists(tmp):
-            os.remove(tmp)
-
-        if self._fid_meta_partition_json is not None:
-            self.drive.download_file(local_dir="./", fid=self._fid_meta_partition_json)
-        if not os.path.exists(tmp):
-            return {}
-        with open(tmp, "r") as f:
-            return json.load(f)
+            return json.load(open(tmp, "r"))
+        else:
+            return []
