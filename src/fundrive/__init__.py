f7c1e21d3b4258a6de202ad18350583db7e57687
farfun <1007530194@qq.com>
2024-11-26 15:14:58 +0800

    add

--- src/fundrive/__init__.py
+++ src/fundrive/__init__.py
@@ -6,6 +6,7 @@ from .drives import (
     OSSDrive,
     TianChiDrive,
     TSingHuaDrive,
+    ZenodoDrive,
 )
 from .fungit import GiteeDrive, GithubDrive
 
@@ -14,6 +15,7 @@ __all__ = [
     "OSSDrive",
     "TianChiDrive",
     "TSingHuaDrive",
+    "ZenodoDrive",
     "OpenDataLabDrive",
     "GithubDrive",
     "LanZouDrive",
--- src/fundrive/drives/__init__.py
+++ src/fundrive/drives/__init__.py
@@ -7,12 +7,14 @@ from .tianchi import TianChiDrive
 from .tsinghua import TSingHuaDrive
 from .tsinghua import download as download_tsinghua
 from .wenshushu import WSSDrive
+from .zenodo import ZenodoDrive
 
 __all__ = [
     "TSingHuaDrive",
     "download_tsinghua",
     "TianChiDrive",
     "OSSDrive",
+    "ZenodoDrive",
     "OpenXLabDrive",
     "OpenDataLabDrive",
     "LanZouDrive",
