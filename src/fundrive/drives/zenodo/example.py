6264eccf13d9e8173b71e38b9028f332cbf7b990
farfun <1007530194@qq.com>
2024-12-03 15:25:46 +0800

    add

--- src/fundrive/drives/zenodo/example.py
+++ src/fundrive/drives/zenodo/example.py
@@ -1,5 +1,6 @@
-from fundrive.drives import ZenodoDrive
 from funsecret import read_secret
+
+from fundrive.drives import ZenodoDrive
 
 drive = ZenodoDrive()
 drive.login(access_token=read_secret("fundrive", "zenodo", "access_token"))
@@ -9,3 +10,6 @@ for file in drive.get_file_list("13293150"):
     print(file)
 # 下载一个record下的所有文件
 drive.download_file(record_id="13293150", local_dir="./resource/songs")
+record_id = drive.mkdir()["metadata"]["prereserve_doi"]["recid"]
+
+drive.upload_dir(local_path="./resource/songs", record_id=record_id)
